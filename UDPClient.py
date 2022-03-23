3afea421e73351243b6d2c361632962fae3c78e9
akivaab <amabramo18@gmail.com>
2022-03-23 19:25:16 +0200

    use snake_case

--- UDPClient.py
+++ UDPClient.py
@@ -1,32 +1,32 @@
 from socket import *
 
 
-def sendMessage():
+def send_message():
     server_name = 'hostname'  # replace with IP address of server
     server_port = 12000
     client_socket = socket(AF_INET, SOCK_DGRAM)
-    message = getMessage1()  # can replace with getMessage2() or getMessage3()
+    message = get_message1()  # can replace with get_message2() or get_message3()
     client_socket.sendto(message.encode(), (server_name, server_port))
     modified_message, server_address = client_socket.recvfrom(2048)
     print(modified_message.decode())
     client_socket.close()
 
 
-def getMessage1():
+def get_message1():
     message = input("Input lowercase sentence: ")
     return message
 
 
-def getMessage2():
+def get_message2():
     num1 = int(input("Enter a number: "))
     num2 = int(input("Enter another number: "))
     message = ",".join([str(num1), str(num2)])
     return message
 
 
-def getMessage3():
+def get_message3():
     message = input("Input name: ")
     return message
 
 
-sendMessage()
+send_message()
--- UDPServer.py
+++ UDPServer.py
@@ -1,33 +1,33 @@
 from socket import *
 
 
-def receiveMessage():
+def receive_message():
     server_port = 12000
     server_socket = socket(AF_INET, SOCK_DGRAM)
     server_socket.bind(('', server_port))
     print("Ready to receive.")
     while True:
         message, client_address = server_socket.recvfrom(2048)
-        modified_message = modifyMessage1(message.decode())  # can replace with modifyMessage2() or modifyMessage3()
+        modified_message = modify_message1(message.decode())  # can replace with modify_message2() or modify_message3()
         server_socket.sendto(modified_message.encode(), client_address)
 
 
-def modifyMessage1(message):
+def modify_message1(message):
     modified_message = message.upper()
     return modified_message
 
 
-def modifyMessage2(message):
+def modify_message2(message):
     numbers = message.split(",")
     num_sum = int(numbers[0]) + int(numbers[1])
     return str(num_sum)
 
 
-def modifyMessage3(message):
+def modify_message3(message):
     names = ["Zero", "One", "Two", "Three", "Four", "Five"]
     numbers = [0, 1, 2, 3, 4, 5]
     index = names.index(message)
     return str(numbers[index])
 
 
-receiveMessage()
+receive_message()
